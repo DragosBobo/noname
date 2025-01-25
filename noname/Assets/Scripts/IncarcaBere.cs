db9c5c622d497d1b7fbae4c7d62c713004f0d8cc
ChiriacAlexandru <tagestudio.alexandru@gmail.com>
2025-01-25 20:31:55 +0200

    schimbari pe partea de spawnat bere etc

---
+++ noname/Assets/Scripts/IncarcaBere.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class IncarcaBere : MonoBehaviour
+{
+    public bool EsteBere;
+
+
+}
---
+++ noname/Assets/Scripts/TargetScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    public bool isTriggered = false;
+    public GameObject prefabToSpawn; // Prefabul care va fi instanțiat
+    public Transform spawnLocation;  // Locația unde va fi spawnat obiectul
+
+    void Update()
+    {
+        if (isTriggered)
+        {
+            // Codul pentru ce se întâmplă când variabila devine true
+            Debug.Log("Triggered!");
+
+            // Verifică dacă există prefab-ul și locația
+            if (prefabToSpawn != null && spawnLocation != null)
+            {
+                // Spawnează prefab-ul la locația dorită
+                Instantiate(prefabToSpawn, spawnLocation.position, spawnLocation.rotation);
+            }
+
+            // Oprește spawn-ul pentru a nu se întâmpla la fiecare frame
+            isTriggered = false; // Sau poți pune o altă condiție de resetare
+        }
+    }
+}
