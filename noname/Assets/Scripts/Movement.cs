5d320aa60ab1611aa03c4023272a4658a70a47be
ChiriacAlexandru <tagestudio.alexandru@gmail.com>
2025-01-25 14:50:32 +0200

    schimbari movement si particule bule

--- noname/Assets/Scripts/Movement.cs
+++ noname/Assets/Scripts/Movement.cs
@@ -9,6 +9,7 @@ public class Movement : MonoBehaviour
     public InputController input;
     public Animator animator;
     public Transform visualTransform;
+    public CameraScript cameraScript;
 
     [Header("Valori")]
     public float walkSpeed = 5;
@@ -47,7 +48,6 @@ public class Movement : MonoBehaviour
 
     }
 
-
     private void FixedUpdate()
     {
         moveController();
@@ -61,7 +61,8 @@ public class Movement : MonoBehaviour
     {
         moveValue = input.moveDirection;
 
-        if (isDrunk)
+        Vector3 forward = cameraScript.GetCameraForward();
+        Vector3 right = cameraScript.GetCameraRight(); if (isDrunk)
         {
             moveSpeed = drunkSpeed;
         }
@@ -74,7 +75,7 @@ public class Movement : MonoBehaviour
             moveSpeed = walkSpeed;
         }
 
-        Vector3 moveDirection = new Vector3(moveValue.x, 0, moveValue.y).normalized;
+        Vector3 moveDirection = forward * moveValue.y + right * moveValue.x;
         Vector3 targetPosition = rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime;
         rb.MovePosition(targetPosition);
 
